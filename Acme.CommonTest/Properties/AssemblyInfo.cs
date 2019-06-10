8267625b166b2ea74aec5b1909b22703d66b4ff7
Jfleming <Jfleming07712@gmail.com>
2019-06-10 13:53:01 -0400

    Mod 9 complete in the OOP tutorial.  Moving on to interfaces mod now.

---
+++ Acme.CommonTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Acme.CommonTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Acme.CommonTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b7e7ccf2-f269-4ee6-8bca-d9ca77e2b7fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
